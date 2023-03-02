0c2eceb884eb1927f01f2a4b17482431b270ed58
Djordje <djordjemilic33@gmail.com>
2023-03-02 13:34:37 +0100

    added footer

---
+++ components/Footer.jsx
@@ -0,0 +1,61 @@
+import styles from "../styles/Footer.module.scss";
+import Link from "next/link";
+import { motion } from "framer-motion";
+
+const variants = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+      delay: 1,
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+      delay: 1,
+    },
+  },
+};
+const Footer = () => {
+  return (
+    <motion.footer
+      className={styles.container}
+      variants={variants}
+      initial="hidden"
+      animate="visible"
+      exit="hidden"
+    >
+      <ul>
+        <li>
+          <Link href="">
+            <a>Fb</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="">
+            <a>Tw</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="">
+            <a>In</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="">
+            <a>Yt</a>
+          </Link>
+        </li>
+      </ul>
+      <div className={styles.copyright}>
+        <span>COPYRIGHT @ 2022.</span>
+      </div>
+    </motion.footer>
+  );
+};
+
+export default Footer;
